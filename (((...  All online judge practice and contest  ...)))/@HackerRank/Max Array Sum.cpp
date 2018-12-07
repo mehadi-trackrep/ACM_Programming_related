5ba3389a9ed18ccedaa32690ba14c3dc25044040
mehadi-trackrep <mehadi541@gmail.com>
2018-12-07 18:21:32 +0600

    dp max subset of array updated

--- (((...  All online judge practice and contest  ...)))/@HackerRank/Max Array Sum.cpp
+++ (((...  All online judge practice and contest  ...)))/@HackerRank/Max Array Sum.cpp
@@ -10,23 +10,25 @@ int dp[sz][2];
 
 int dp_func(int pos, int ck){
     if(pos > n) return 0;
+    if(vis[pos][ck]) return dp[pos][ck];
+
     int ret = INT_MIN;
 
     if(ck == 0) ret = ara[pos] + dp_func(pos+1, 1); /// false(0) means immediate adjacent position already took
     ret = max(ret, dp_func(pos+1, 0));
-
+    vis[pos][ck] = 1;
     return dp[pos][ck] = ret;
 }
 
 int main(){                 ///AC
     int mx = INT_MIN;
     memset(vis, 0, sizeof(vis));
-    cin  >> n;
-    for(int i=1; i<=n; i++) cin >> ara[i];
+    scanf("%d",&n);
+    for(int i=1; i<=n; i++) scanf("%d",&ara[i]);
 
     mx = max(mx, dp_func(1,0));
 
-    cout << mx << endl;
+    printf("%d\n",mx);
 
     return 0;
 }
